3c50dc52d9b1203592b2f2d8e1e4220773394b86
Carson <carsonbcobb@gmail.com>
2025-04-30 13:37:05 -0600

    july 2025 poster

--- src/sections/home/home.jsx
+++ src/sections/home/home.jsx
@@ -17,7 +17,7 @@ import notes from '../../assets/home/notes.png';
 import SimpleSlider from '../../components/slider/slider.jsx';
 import SelectNav from '../../components/selectNav/selectNav';
 
-import upcomingPoster from '../../assets/home/concert_1_2025.png'
+import upcomingPoster from '../../assets/home/july_2025.png'
 import upcomingPosterSub from '../../assets/home/parkingmap.png'
 
 import VideoBackgroundComponent from '../../components/backgroundVideo/BackgroundVideo.jsx';
