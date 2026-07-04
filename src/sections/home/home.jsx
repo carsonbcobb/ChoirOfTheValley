d26e21c760c8af5f2d580c71e90856f3ebe34fb8
carsonbcobb <carsonbcobb@gmail.com>
2026-07-04 11:50:09 -0600

    image swap

--- src/sections/home/home.jsx
+++ src/sections/home/home.jsx
@@ -17,7 +17,7 @@ import notes from '../../assets/home/notes.png';
 import SimpleSlider from '../../components/slider/slider.jsx';
 import SelectNav from '../../components/selectNav/selectNav';
 
-import upcomingPoster from '../../assets/home/summer-text-flyer.jpeg';
+import upcomingPoster from '../../assets/home/summer-text-flyer.png';
 import upcomingPosterSub from '../../assets/home/parkingmap.png';
 
 import VideoBackgroundComponent from '../../components/backgroundVideo/BackgroundVideo.jsx';
